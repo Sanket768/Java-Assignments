e6fb6ab436a57be7ed158e7e6a13ae7ade06b46f
VratikZ02 <vratik.zade@pinnacle.in>
2025-01-06 20:25:20 +0530

    edited twentytwo file

--- Vratik/ClassObjects/twentytwo.java
+++ Vratik/ClassObjects/twentytwo.java
@@ -1,43 +1,12 @@
 /*Create a Department class and an Employee class. Represent the "has-a" relationship. */
 class Department {
-    private String name;
-    private Employee[] employees;
-
-    public Department(String name, int numEmployees) {
-        this.name = name;
-        this.employees = new Employee[numEmployees];
-    }
-
-    public void addEmployee(Employee e) {
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] == null) {
-                employees[i] = e;
-                return;
-            }
-        }
-    }
-
-    public void displayDepartment() {
-        System.out.println("Department: " + name);
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] != null) {
-                employees[i].displayEmployee();
-            }
-        }
-    }
+    
 
 }
 
 public class twentytwo {
     public static void main(String[] args) {
         
-        /*
-         * System.out.println("Name: " + name);
-        System.out.println("Department: " + department);
-        System.out.println("Salary: " + salary);
-        System.out.println("Years of Service: " + yearsOfService);
-        System.out.println("Employee ID: " + employeeId);
-        System.out.println("Employee Type: " + employeeType);
-         */
+        
     }
 }
