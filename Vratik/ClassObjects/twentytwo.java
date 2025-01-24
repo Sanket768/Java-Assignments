57aa3f553e14a7f24f56d12be06e4a93134491d3
vizzscript <vratik.zade.15@gmail.com>
2025-01-24 15:11:29 +0530

    Resolved conflicts

--- Vratik/ClassObjects/twentytwo.java
+++ Vratik/ClassObjects/twentytwo.java
@@ -1,33 +1,41 @@
+
 /*
  * Aggregation is a "has-a" relationship where one class contains another, 
  * but contained object can exist independently
  */
-class Department{
+class Department {
     private String name;
 
-    public Department(String name){
-        this.name = name;
-    }
-    public String getNa(){
-        return name;
-    }
-}
-class Employee{
-    private String name;
-
-    public Employee(String name){
+    public Department(String name) {
         this.name = name;
     }
 
-    public String getName(){
+    public String getNa() {
         return name;
     }
 }
+
+/*
+ * Create a Department class and an Employee class. Represent the "has-a"
+ * relationship.
+ */
+class Employee {
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
 public class twentytwo {
     public static void main(String[] args) {
         Department dp = new Department("Technical");
         Employee emp = new Employee("Aryan Chaple");
-        System.out.println(emp.getName() + " is from "+ dp.getNa()+" department.");
+        System.out.println(emp.getName() + " is from " + dp.getNa() + " department.");
 
-}
+    }
 }
